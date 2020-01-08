917f17557bdbc952324cc34b114d81f6314a006d
Parth Chugh <parthchugh52@gmail.com>
2020-01-08 16:43:49 +0530

    added one random function to test react

--- client/components/Hello/Hello.js
+++ client/components/Hello/Hello.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 require('./Hello.scss');
 
-var Hello = React.createClass({
-  render() {
-    return <h1>Hello, {this.props.name}!</h1>;
-  }
-});
+const Hello = (props) =>  {
+  useEffect(() => {
+    const url = "http://192.168.1.4:5000/getRandomData"
+    async function getRandomData () {
+      fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then(json => {
+          console.log(json) 
+        })
+        .catch(() => {
+        });
+    } 
+    getRandomData();
+  },[]);
+
+  return(
+    <div>
+      <h1>Hello, {props.name}!</h1>
+    </div>
+  ) 
+}
 
 export default Hello;
--- client/components/HelloForm/HelloForm.js
+++ client/components/HelloForm/HelloForm.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Hello from '../Hello/Hello';
 
-var HelloForm = React.createClass({
-  getInitialState() {
-    return {
-      name: 'worlds'
-    };
-  },
-
-  render() {
-    return (
-      <div className="hello-form">
-        <input type="text" onChange={this.onChange} />
-        <Hello name={this.state.name} />
-      </div>
-    );
-  },
-
-  onChange: function(e) {
-    this.setState({
-      name: e.target.value
-    });
-  }
-});
+const HelloForm = () => {
+  return (
+    <div className="hello-form">
+      <Hello name={"Parth"} />
+    </div>
+  );
+};
 
 export default HelloForm;
